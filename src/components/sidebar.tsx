beda556cc7f69bc4d4ccc6e1803455975fca05c4
arjunjoshua <arjunjoshua@gmail.com>
2023-07-06 19:42:28 +0100

    Added sidebar navigation to select lobbies. Still has bugs, do not push to main.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,47 +1,61 @@
-import { useState, useEffect } from 'react';
-import Sidebar from 'react-sidebar';
+import React, { useState, useEffect } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import '../styles/burger-menu.css'
 
 interface LobbySidebarProps {
-    handleLobbySelect: (lobbyId: string) => void;
-  }
+  handleLobbySelect: (lobbyId: string) => void;
+}
 
 interface Lobby {
-    _id: string;
-    name: string;
-    trainers: string[];
+  _id: string;
+  name: string;
+  trainers: string[];
 }
-  
- const LobbySidebar: React.FC<LobbySidebarProps> = ({ handleLobbySelect }) => {
-    const [lobbies, setLobbies] = useState<Lobby[]>([]);
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-  
-    const fetchLobbies = async () => {
-      const response = await fetch('http://localhost:3000/lobbies');
-      const data = await response.json();
-      setLobbies(data);
-    }
-  
-    const onSetSidebarOpen = (open: boolean) => {
-      setSidebarOpen(open);
-    }
-  
-    useEffect(() => {
-      fetchLobbies();
-    }, []);
-  
-    return (
-      <Sidebar
-        sidebar={lobbies.map(lobby => <button key={lobby._id} onClick={() => handleLobbySelect(lobby._id)}>{lobby.name}</button>)}
-        open={sidebarOpen}
-        onSetOpen={onSetSidebarOpen}
-        styles={{ sidebar: { background: "gray" } }}
-      >
-        <button onClick={() => onSetSidebarOpen(true)}>
-          Lobbies
-        </button>
-      </Sidebar>
-    );
-  };
 
-  export default LobbySidebar;
+const LobbySidebar: React.FC<LobbySidebarProps> = ({ handleLobbySelect }) => {
+  const [lobbies, setLobbies] = useState<Lobby[]>([]);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const fetchLobbies = async () => {
+    const response = await fetch('http://localhost:3000/lobbies');
+    const data = await response.json();
+    setLobbies(data);
+  }
+
+  const handleStateChange = (state: {isOpen: boolean}) => {
+    setSidebarOpen(state.isOpen);
+}
+
+  const closeMenu = () => setSidebarOpen(false);
+  
+  const handleButtonClick = (lobbyId: string) => {
+    handleLobbySelect(lobbyId);
+    closeMenu();
+  }
+
+  useEffect(() => {
+    fetchLobbies();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setSidebarOpen(true)}>
+        Open menu
+      </button>
+
+      <Menu 
+        isOpen={sidebarOpen} 
+        onStateChange={(state) => handleStateChange(state)}
+      >
+        {lobbies.map(lobby => (
+          <button key={lobby._id} onClick={() => handleButtonClick(lobby._id)}>
+            {lobby.name}
+          </button>
+        ))}
+      </Menu>
+    </div>
+  );
+};
+
+export default LobbySidebar;
   
--- src/components/trainerDropdown.tsx
+++ src/components/trainerDropdown.tsx
@@ -23,6 +23,7 @@ interface TrainerDropdownProps {
 const TrainerDropdown: React.FC<TrainerDropdownProps> = ({ trainers, selectedTrainers, setSelectedTrainers }) => {
   const handleChange = (index: number) => (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedTrainerId = event.target.value;
+    console.log("Hi");
     const newSelectedTrainer = trainers.find(trainer => trainer._id === selectedTrainerId);
     const newSelectedTrainers = [...selectedTrainers];
     newSelectedTrainers[index] = newSelectedTrainer || null;
