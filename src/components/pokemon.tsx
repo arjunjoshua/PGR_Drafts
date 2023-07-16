f1c90ee028124af9b2851bb14e87d9700d4a0064
arjunjoshua <arjunjoshua@gmail.com>
2023-07-16 13:37:24 +0100

    Improved logic for pokemon card and added shinies!

--- src/components/pokemon.tsx
+++ src/components/pokemon.tsx
@@ -5,12 +5,30 @@ type PokemonProps = {
 }
 
 const Pokemon: React.FC<PokemonProps> = ({ pokemonName }) => {
-    return (   
+    const BASE_URL = 'https://img.pokemondb.net/sprites';
+    const pokemonNameCapitalized = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+
+    let folder = 'home/normal';
+
+    //shiny chance!
+    const shinyChance = Math.floor((Math.random() * 100) / 2);
+    if (shinyChance === 6) {
+        folder = 'home/shiny';
+    }
+
+    //prevents mewtwo-armored from breaking
+    if (pokemonName === 'mewtwo-armored') {
+        folder = 'go/normal';
+    }
+
+    const imgSrc = `${BASE_URL}/${folder}/${pokemonName}.png`;
+
+    return (
         <div className="pokemon-card">
-            <img src={`https://img.pokemondb.net/sprites/home/normal/${pokemonName}.png`} alt={pokemonName} />
-            <h2>{pokemonName}</h2>
+            <img src={imgSrc} alt={pokemonName} />
+            <h2>{pokemonNameCapitalized}</h2>
         </div>
-    );
+    );   
 };
 
 
