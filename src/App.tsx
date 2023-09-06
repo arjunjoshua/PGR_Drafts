8942464fbe423f827c43bd3c464ba42e55ee81f8
arjunjoshua <arjunjoshua@gmail.com>
2023-09-06 14:33:36 +0100

    Began adding a home page and navbar to the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,24 @@
-import './App.css';
-import TrainerDropdown from './components/trainerDropdown';
-import LobbySidebar from './components/sidebar'; // import the sidebar component
-import { useState, useEffect } from 'react';
-import { Trainer } from './components/trainerDropdown';
-import LoadingSpinner from './components/loadingSpinner';
-import { backend_url } from './constants/constants';
+import './App.css'
+import PvpDraft from './pvpDraft'
+import { BrowserRouter as Router, Route, Routes, Link  } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './components/home';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [trainers, setTrainers] = useState([]);
-  const [selectedTrainers, setSelectedTrainers] = useState<(Trainer | null)[]>([null, null]);
-  const [selectedLobby, setSelectedLobby] = useState<{ _id: string; name: string }>({ _id: '64b3d97ba05427be59779158', name: 'MLC-GrandUnderground' });
-
-  const addPokemonToTrainer = async (teamID: string, pokemonName: string) => {   
-    try {
-        await fetch(`${backend_url}/addPokemon`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                teamID,
-                pokemonName,
-            })
-        });
-
-        // Notify the user that the request was successful
-        window.alert("Pokémon added successfully! Click OK or refresh to see the changes.");
-        // Refetch the lobby data or handle the response as needed
-        handleLobbySelect(selectedLobby); // for instance
-    } catch (error) {
-        console.error("Error adding Pokémon:", error);
-    }
-  };
-
-  const removePokemonFromTrainer = async (teamID: string, pokemonName: string) => {
-    console.log(teamID, pokemonName);
-    try {
-        await fetch(`${backend_url}/removePokemon`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                teamID,
-                pokemonName,
-            })
-        });
-        
-        // Notify the user that the request was successful
-        window.alert("Pokémon removed successfully! Click OK or refresh to see the changes.");
-        // Refetch the lobby data or handle the response as needed
-        handleLobbySelect(selectedLobby); // for instance
-    } catch (error) {
-        console.error("Error removing Pokémon:", error);
-    } 
-  };
-  
-  const handleLobbySelect = async (lobby: { _id: string; name: string }) => {
-        setLoading(true);
-        const response = await fetch(`${backend_url}/lobby/${lobby._id}`);
-        const data = await response.json();
-        setTrainers(data.trainers);
-        setSelectedTrainers([data.trainers[0], data.trainers[1]]);
-        setSelectedLobby(data.lobby);
-        setLoading(false);
-      }
-
-      useEffect(() => {
-        setLoading(true);
-        fetch(`${backend_url}/lobby/64b3d97ba05427be59779158`)
-          .then(response => response.json())
-          .then(data => {
-            setTrainers(data.trainers);
-            setSelectedTrainers([data.trainers[0], data.trainers[1]]);
-            setLoading(false);
-          }) 
-          .catch(error => {
-            console.error(error);
-            setLoading(false);
-          });
-      }, []);
-
-  if (loading) {
-   return <LoadingSpinner/>
-  }
-  
-  return (
-    <div className='app-body'>
-      <LobbySidebar handleLobbySelect={handleLobbySelect} selectedLobbyID={selectedLobby._id}/>
-      <h1>{selectedLobby?.name}</h1>
-      <TrainerDropdown trainers={trainers} selectedTrainers={selectedTrainers} 
-      setSelectedTrainers={setSelectedTrainers} selectedLobby={selectedLobby} 
-      addPokemonToTrainer={addPokemonToTrainer} removePokemonFromTrainer={removePokemonFromTrainer}
-      />
-    </div>
-  );
+    return (
+        <Router>
+            <div className='app-body'> 
+            <div>
+            <Navbar />  
+            </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pvp" element={<PvpDraft />} />
+                    {/*<Route path="/raid" element={<RaidDraft />} />*/}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import '../styles/navbar.css';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+
+        <nav className="navbar">
+            <Link to="/pvp">PvP Draft</Link>
+            <Link to="/raid">Raid Draft</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
