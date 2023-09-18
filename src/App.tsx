8de93035e231d9be350c0c6bcaba5a9a03c8e7a5
arjunjoshua <arjunjoshua@gmail.com>
2023-09-18 11:42:12 +0100

    Added a button to view scoreboard.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from 'react';
 import { Trainer } from './components/trainerDropdown';
 import LoadingSpinner from './components/loadingSpinner';
 import { backend_url } from './constants/constants';
+import ScoreboardButton from './components/scoreboardButton';
+import ReportResultButton from './components/reportResultButton';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -115,7 +117,11 @@ const handleRefresh = async (lobby: { _id: string; name: string }, selectedTrain
   
   return (
     <div className='app-body'>
+      <div className='top-container'>
       <LobbySidebar handleLobbySelect={handleLobbySelect} selectedLobbyID={selectedLobby._id}/>
+      <ScoreboardButton/>
+      <ReportResultButton/>
+      </div>
       <h1>{selectedLobby?.name}</h1>
       <TrainerDropdown trainers={trainers} selectedTrainers={selectedTrainers} 
       setSelectedTrainers={setSelectedTrainers} selectedLobby={selectedLobby} 
