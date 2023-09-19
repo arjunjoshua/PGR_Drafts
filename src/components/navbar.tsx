041c10eaa251a4ac2edbd3595722a54ec6dfe128
arjunjoshua <arjunjoshua@gmail.com>
2023-09-19 20:20:43 +0100

    Added a navbar component and a navbar css file. Requires more styling to be fully operational.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,22 @@
 import { Link } from 'react-router-dom';
+import '../styles/navbar.css'
 
 function Navbar() {
     return (
-        <nav>
+        
+        <nav className='navbar'>
+            <Link to="/">
+                <div className='logo-container'>
+                    <img src='yourcalc.png' alt="PGR Logo" className='navbar-logo'/>
+                    <h3 className='navbar-title'>PoGo Raids</h3>
+                </div>
+            </Link>
+            <div className='navbar-container'>
             <ul>
                 <li><Link to="/pvp-draft-teams">PvP Draft Teams</Link></li>
                 <li><Link to="/points-table">Points Table</Link></li>
             </ul>
+            </div>
         </nav>
     )
 }
