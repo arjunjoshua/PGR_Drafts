ddb7fe043c23ec68e5db14cd49fcfa01ec929b24
arjunjoshua <arjunjoshua@gmail.com>
2025-01-17 12:45:00 +0000

    Added a new function to handle edge cases for dialga and palkia specifically

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -5,11 +5,15 @@ type TeamProps = {
     team: string[]; // An array of Pokemon names
 }
 
+function handle_replacements(name: string): string {
+    return name.replace("dialga-altered", "dialga").replace("palkia-altered","palkia")
+}
+
 const Team: React.FC<TeamProps> = ({ team }) => {
     return (
         <div className='pokemon-grid'>
             {team.map((pokemonName, index) => (
-                <Pokemon key={index} pokemonName={pokemonName.trim()} />
+                <Pokemon key={index} pokemonName={handle_replacements(pokemonName.trim())} />
             ))}
         </div>
     );
